a3ff43f400f84276510a903740b0b8c9d5f7a786
kducasse <66579798+kducasse@users.noreply.github.com>
2020-08-11 16:25:51 -0400

    Delete main.js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./components/App"
-import PetTypesIndex from "./components/PetTypesIndex"
-
-ReactDOM.render(
-  <App />, 
-  document.getElementById("app")
-)
