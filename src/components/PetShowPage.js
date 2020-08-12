4472c4fa515dd8c7f3c124aec95a8fdf9ab04c50
kducasse <karaducasse@gmail.com>
2020-08-12 13:04:16 -0400

    created the show page component

--- src/components/PetShowPage.js
+++ src/components/PetShowPage.js
@@ -1,6 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import AdoptionForm from "./AdoptionForm"
+
 const PetShowPage = (props) => {
-  return(
-    <div>This is the pet show page</div>
+  const speciesId = props.match.params.species
+  const adoptablePetId = props.match.params.id
+
+  const [adoptablePet, setAdoptablePet] = useState({})
+  const [isAdopting, setIsAdopting] = useState(false)
+  const [animalPageFound, setAnimalPageFound] = useState(true)
+
+  useEffect(() => {
+    fetch(`/api/v1/adoptable_pets?type=${speciesId}&id=${adoptablePetId}`)
+      .then(response => {
+        if (response.ok) {
+          return response
+        } else {
+          setAnimalPageFound(false)
+        }
+      })
+      .then(response => response.json())
+      .then(adoptablePet => setAdoptablePet(adoptablePet.rows))
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  const handleButtonClick = event => {
+    event.preventDefault()
+    setIsAdopting(true)
+  }
+  const animalInformation = (
+    <div>
+      <p>Name {adoptablePet.name}</p>
+      <p>Age {adoptablePet.age}</p>
+      <p>Vaccination Status {adoptablePet.vaccination_status}</p>
+      <p>Adopt me: {adoptablePet.adoption_story}</p>
+    </div>
+  )
+
+  let form = ""
+  let submitButton = (
+    <form onSubmit={handleButtonClick}>
+      <input className="button" type="submit" value="Adopt Me!" />
+    </form>
+  )
+
+  if (isAdopting) {
+    form = AdoptionForm
+    submitButton = ""
+  }
+
+  let error
+  if (!animalPageFound) {
+    error = "404 Error.  Animal is not Found."
+    submitButton = ""
+    animalInformation = ""
+    form = ""
+  }
+
+  return (
+    <>
+      {error}
+      {animalInformation}
+      {submitButton}
+      {form}
+
+    </>
   )
 }
 
