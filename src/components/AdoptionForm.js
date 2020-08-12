8acb5b516367e9e104e61aa11b701d70bcf29316
MtnGreen <patricemahoney@gmail.com>
2020-08-12 12:47:34 -0400

    fixed form for adoption and minor fix to new pet form

--- src/components/AdoptionForm.js
+++ src/components/AdoptionForm.js
@@ -8,6 +8,8 @@ const AdoptionForm = props => {
     homeStatus: "default"
   })
 
+  const [adoptAppStatus, setAdoptAppStatus] = useState("");
+
   const handleAppChange = event => {
     setNewApplication({
       ...newApplication,
@@ -24,35 +26,65 @@ const AdoptionForm = props => {
       homeStatus: newApplication.homeStatus
     }
 
-    setNewApplication({
-      name: "",
-      phoneNumber: "",
-      email: "",
-      homeStatus: ""
+
+    let filledOut = true;
+
+    let newAdoptApp = Object.keys(payload);
+    newAdoptApp.forEach(key => {
+      if (newApplication[key] === "") {
+        filledOut = false
+      }
     })
+
+    if (filledOut) {
+      fetch("/api/v1/adoption_application", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      })
+        .then((result) => {
+          if(result.ok){
+          setAdoptAppStatus("Your request is in process")}
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+    }
   }
 
-  return (
-    <form onSubmit={handleAppSubmit}>
-      <label htmlFor="name">Your Name:
+  let adoptForm;
+  if (adoptAppStatus !== "Your request is in process") {
+    adoptForm = (
+      <form onSubmit={handleAppSubmit}>
+        <label htmlFor="name">Your Name:
         <input type="text" name="name" id="name" onChange={handleAppChange} value={newApplication.name} />
-      </label>
-      <label htmlFor="phoneNumber">Phone Number:
+        </label>
+        <label htmlFor="phoneNumber">Phone Number:
         <input type="text" name="phoneNumber" id="phoneNumber" onChange={handleAppChange} value={newApplication.phoneNumber} />
-      </label>
-      <label htmlFor="email">Email Address:
+        </label>
+        <label htmlFor="email">Email Address:
         <input type="text" name="email" id="email" onChange={handleAppChange} value={newApplication.email} />
-      </label>
-      <label htmlFor="homeStatus">Select Home Status</label>
-      <select name="homeStatus" id="homeStatus" onChange={handleAppChange} value={newApplication.homeStatus}>
-        <option value="default" disabled hidden>Select Home Status</option>
-        <option value="own">Own</option>
-        <option value="rent">Rent</option>
-      </select>
-      <div>
-        <input className="button" type="submit" value="Submit" />
-      </div>
-    </form>
+        </label>
+        <label htmlFor="homeStatus">Select Home Status</label>
+        <select name="homeStatus" id="homeStatus" onChange={handleAppChange} value={newApplication.homeStatus}>
+          <option value="default" disabled hidden>Select Home Status</option>
+          <option value="own">Own</option>
+          <option value="rent">Rent</option>
+        </select>
+        <div>
+          <input className="button" type="submit" value="Submit" />
+        </div>
+      </form>
+    )
+  }
+  return (
+    <>
+      {adoptAppStatus}
+      {adoptForm}
+    </>
   )
 }
 
