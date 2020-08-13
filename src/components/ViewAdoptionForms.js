459741b6e13695738c78b0895ad72797ccea1d89
J Pineda <57915817+Krabby-Patties@users.noreply.github.com>
2020-08-13 12:07:43 -0700

    added a way to view adoptionform applications

--- src/components/ViewAdoptionForms.js
+++ src/components/ViewAdoptionForms.js
@@ -4,9 +4,14 @@ import AdoptionForm from './AdoptionForm'
 const ApprovalForm = props => {
     const [newApproval, setNewApproval] = useState("")
     const [allAdoptionForms, setAllAdoptionForms] = useState([])
+    const [currentlySelectedForm, setCurrentlySelectedForm] = useState(null)
 
     const handleApprovalChange = event => {
         setNewApproval(event.currentTarget.value)
+    }
+
+    const handleSelectionChange = event => {
+        setCurrentlySelectedForm(JSON.parse(event.currentTarget.value))
     }
 
     const handleApprovalSubmit = event => {
@@ -16,31 +21,53 @@ const ApprovalForm = props => {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
+            },
+            body: {
+                petId:currentlySelectedForm.pet_id,
+                applicationId:currentlySelectedForm.id,
+                approvalStatus:newApproval
             }
         })
     };
 
     useEffect(() => {
         fetch("/api/v1/adoption_application").then((response) => response.json())
-            .then((adoptionForms) => {
+            .then(adoptionForms => {
                 setAllAdoptionForms(adoptionForms.rows)
-            })
-    }
+            })}
         , [])
 
-        let allForms = allAdoptionForms.map(AdoptionForm => {
-            return <option value="">--{`${AdoptionForm.name}, Application#${AdoptionForm.id}, ${AdoptionForm.application_status}`}--</option>
-        })
+    let allForms = allAdoptionForms.map(AdoptionForm => {
+        return <option key={AdoptionForm.id} value={JSON.stringify(AdoptionForm)}>--{`${AdoptionForm.name}, Application#${AdoptionForm.id}, ${AdoptionForm.application_status}`}--</option>
+    })
+
+    let formDisplay
+    if (currentlySelectedForm !== null) {
+        formDisplay = (
+            <div>
+                <ul>
+                    <li>{`Applicant Id# ${currentlySelectedForm.id}`}</li>
+                    <li>{`Pet Id# ${currentlySelectedForm.pet_id}`}</li>
+                    <li>{`Applicant Name:${currentlySelectedForm.name}`}</li>
+                    <li>{`Phone#: ${currentlySelectedForm.phone_number}`}</li>
+                    <li>{`Email: ${currentlySelectedForm.email}`}</li>
+                    <li>{`Applicant's Home Status: ${currentlySelectedForm.home_status}`}</li>
+                    <li>{`Application Status: ${currentlySelectedForm.application_status}`}</li>
+                </ul>
+            </div>)
+    }
+
     return (
-        <form >
-            <label for="completedForms">Select a form to reveiw:</label>
-            <select name="completedForms" id="completedForms">
-                <option value="">--Please choose an option--</option>
+        <form onSubmit={handleApprovalSubmit} >
+            <label htmlFor="completedForms">Select a form to reveiw:</label>
+            <select onChange={handleSelectionChange} name="completedForms" id="completedForms">
+                <option value="null">--Please choose an option--</option>
                 {allForms}
             </select>
 
+            {formDisplay}
 
-            <label for="approval">Choose Approval Status:</label>
+            <label htmlFor="approval">Choose Approval Status:</label>
             <select onChange={handleApprovalChange} value={newApproval} name="approval" id="approval">
                 <option value="">--Please choose an option--</option>
                 <option value="Approve">Approve</option>
@@ -50,3 +77,5 @@ const ApprovalForm = props => {
             <input type="submit" value="Submit" />
         </form >)
 }
+
+export default ApprovalForm
