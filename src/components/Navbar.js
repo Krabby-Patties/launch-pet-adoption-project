522a549932e1fb08c8ab0ce4627835bdf1de9f34
MtnGreen <patricemahoney@gmail.com>
2020-08-13 16:50:10 -0400

    styling changes to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,38 @@ import AvailablePetsIndex from "./AvailablePetsIndex"
 import NewPetForm from "./NewPetForm"
 import PetTypesIndex from "./PetTypesIndex"
 import PetShowPage from "./PetShowPage"
-
 const Navbar = props => {
   return (
     <>
-      <div className="navbar">
-        <Link to="/pets">Home</Link>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="dropdown menu" data-dropdown-menu>
+            <li className="menu-text">
+              Pet Adoption 
+            </li>
+            <li>
+              <div className="navbar">
+                <Link to="/pets">Home</Link>
+              </div>
+            </li>
+            <li>
+              <div className="navbar">
+                <Link to="/pets/1">Two Legged Pets</Link>
+              </div>
+            </li>
+            <li>
+              <div className="navbar">
+                <Link to="/pets/2">Four Legged Pets</Link>
+              </div>
+            </li>
+            <li>
+              <div className="navbar">
+                <Link to="/adoptions/new">Put a Pet Up For Adoption</Link>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="navbar">
-        <Link to="/pets/1">Two Legged Pets</Link>
-      </div>
-      <div className="navbar">
-        <Link to="/pets/2">Four Legged Pets</Link>
-      </div>
-      <div className="navbar">
-        <Link to="/adoptions/new">Put a Pet Up For Adoption</Link>
-      </div>
-      <div className="content">
-        <h1 className="page-title">Pet Adoption</h1>
-      </div>
-
       <Switch>
         <Route exact path="/pets" component={PetTypesIndex} />
         <Route exact path="/adoptions/new" component={NewPetForm} />
@@ -33,5 +45,4 @@ const Navbar = props => {
     </>
   );
 };
-
 export default Navbar;
